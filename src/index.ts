d6cd9346cf54629df6c170117ff1b48eeefce968
Rubén <hey@rubenvara.io>
2023-03-08 15:04:21 +0100

    refactor main code to TS

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,60 @@
-import countries from './countries.json';
+// export const hello = (name: string) => `Hello ${name}!`;
 
-const countriesObject = {};
-const codesObject = {};
-countries.forEach(obj => (countriesObject[obj.code] = obj.country));
-countries.forEach(obj => (codesObject[obj.country] = obj.code));
+// import countries from './countries.json';
+import { countries } from './countries';
+
+// types
+type CountryObject = (typeof countries)[number];
+type Code = CountryObject['code'];
+type Country = CountryObject['country'];
+
+// build helper objects
+const countriesObject = <Record<Code, Country>>{};
+const codesObject = <Record<Country, Code>>{};
+
+countries.forEach((obj) => (countriesObject[obj.code] = obj.country));
+countries.forEach((obj) => (codesObject[obj.country] = obj.code));
 
 // return the country name for a given code
-const getCountry = code => countriesObject[code];
+export const getCountry = (code: Code) => countriesObject[code];
+
 // return the code for a given country name
-const getCode = country => codesObject[country];
+export const getCode = (country: Country) => codesObject[country];
+
+type Props = {
+  object?: boolean;
+  extended?: boolean;
+};
 
 // return an array or an object (with codes as keys) of all countries
-function getCountries(props = {}) {
+export function getCountries(props: Props = {}) {
   // default behaviour: object === false, extended === false
   const { object, extended } = props;
-  let allCountries = countries;
 
-  if (!extended) allCountries = countries.filter(obj => !obj.extended);
+  // if !extended, filter out the "isExtended === true" objects
+  const allCountries = !extended ? countries.filter((obj) => !obj.isExtended) : countries;
 
   if (object) {
-    const result = {};
-    allCountries.forEach(obj => (result[obj.code] = obj.country));
-    return result;
+    // if object, build and return the object
+    return allCountries.map((obj) => ({ [obj.code]: obj.country }));
   }
-  return allCountries.map(obj => obj.country);
+
+  // return just the country names
+  return allCountries.map((obj) => obj.country);
 }
 
 // return an array or an object (with countries as keys) of all country codes
-function getCodes(props = {}) {
+export function getCodes(props: Props = {}) {
   const { object, extended } = props;
-  let allCountries = countries;
 
-  if (!extended) allCountries = countries.filter(obj => !obj.extended);
+  // if !extended, filter out the "isExtended === true" objects
+  const allCountries = !extended ? countries.filter((obj) => !obj.isExtended) : countries;
 
   if (object) {
-    const result = {};
-    allCountries.forEach(obj => (result[obj.country] = obj.code));
-    return result;
+    // if object, build and return the object
+    return allCountries.map((obj) => ({ [obj.country]: obj.code }));
   }
-  return allCountries.map(obj => obj.code);
+
+  // return just the country codes
+  return allCountries.map((obj) => obj.code);
 }
